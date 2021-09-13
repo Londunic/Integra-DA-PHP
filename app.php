db8fdfac25dde3cf41863de918d2fdaac9159099
Londunic <69688193+Londunic@users.noreply.github.com>
2021-09-13 17:44:29 -0500

    proyecto

--- app.php
+++ app.php
@@ -1,11 +1,29 @@
 <?php include("seguridad.php"); ?>
-    <html>
+
+<!DOCTYPE html>
+<html>
     <head>
-        <title>Bienvenido al sistema</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- Custom styles for this template -->
+        <link href="starter-template.css" rel="stylesheet">
+
+        <title>Acceso correcto</title>
     </head>
     <body>
-        Hola! <?php echo $_SESSION["user"]; ?><br>
-        <br>
-        <a href="salir.php">Salir del sistema</a>
+        <div class="container">
+            <h1> Bienvenido al Sistema</h1>
+            <br>
+            <p><?php echo $_SESSION["user"]; ?>, su usuario y clave fueron correctos </p>
+            <br>
+            <p>Esto es una pagina web en la que se prueba hacer Log In con los usuarios
+                del directorio activo </p>
+            <br>
+            <a href="salir.php">Salir</a>
+        </div>
     </body>
     </html>
--- index.php
+++ index.php
@@ -1,26 +1,63 @@
-<?php @session_start(); ?>
+<?php
+    session_start(); 
+    
+    require("ldap.php"); 
+    
+    if ( isset($_POST['usuario']) &&  isset($_POST['clave']) ) {
+        $usr = $_POST["usuario"]; 
+        $usuario = mailboxpowerloginrd($usr,$_POST["clave"]); 
+        if($usuario == "0" || $usuario == ''){ 
+            $_SERVER = array(); 
+            $_SESSION = array(); 
+            $_SESSION["errors"] = "Usuario o clave incorrecta"; 
+        }else{ 
+            $_SESSION["user"] = $usuario; 
+            $_SESSION["autentica"] = "SIP"; 
+            header("Location: app.php"); 
+        } 
+    }
 
-    <html>
+
+?>
+
+<!DOCTYPE html>
+<html>
     <head>
-    <title>Acceso por LDAP</title>
-    <body>
-    <form method="post" action="control.php"><br>
-        <table>
-            <tr>
-                <td>Usuario:</td>
-                <td><input type="text" maxlength="50" name="usuario" id="usuario" /></td>
-            </tr>
-            <tr>
-                <td>Clave:</td>
-                <td><input type="password" maxlength="50" name="clave" id="clave" />
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="Entrar"></td>
-            </tr>
-        </table><br>
-        </form>
-    </body>
-    </html>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-<?php exit(); ?>
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- Custom styles for this template -->
+        <link href="starter-template.css" rel="stylesheet">
+
+        <title>Edwar Jose Londoño Correa</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>Bienvenido</h1>
+            <p>Por favor, ingrese los siguientes datos para poder ingresar al sistema</p>
+            <div style="color: red;"> 
+                <?php 
+                    if(isset($_SESSION["errors"])){
+                        echo $_SESSION["errors"];
+                        unset($_SESSION["errors"]);
+                    }
+                ?>    
+            </div>
+
+            <form method="POST">
+                <label for="usuario">Usuario </label>
+                <input type="text" name="usuario" id="usuario"/><br/>
+                <label for="clave">Contraseña</label>
+                <input type="password" name="clave" id="clave"/><br/>
+                <input type="submit" value="Ingresar">
+            </form>
+
+
+
+        </div>
+    </body>
+</html> 
